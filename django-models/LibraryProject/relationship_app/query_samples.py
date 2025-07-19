46b1ecaf5eec2eb082f277c13cb7ed736ae3b78b
mokhethi-k <mokhethikabelo@gmail.com>
2025-07-19 17:35:08 +0200

    correct queries for librarian and books by specific author

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,6 +1,7 @@
 from relationship_app.models import Author, Book, Library, Librarian
 def book_by_author(author_name):
-    books = Book.objects.filter(author__name = author_name)
+    author = Author.objects.get(name=author_name) 
+    books = Book.objects.filter(author=author)     
     for book in books:
         print(book.title)
 
@@ -11,5 +12,6 @@ def All_books(library_name):
         print(book.books)
 
 def librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    print(library.librarian.name)
+    library = Library.objects.get(name = library_name)
+    librarian = Librarian.objects.filter(library=library)
+    print(librarian.name)
