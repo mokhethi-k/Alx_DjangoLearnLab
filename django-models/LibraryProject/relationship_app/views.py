59e6af8649247906cdd8e82209ff251f873c02ef
mokhethi-k <mokhethikabelo@gmail.com>
2025-07-19 19:51:13 +0200

    correct the way of importing the module

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from relationship_app.models import Library
+from .models import Library
 from django.views.generic import DetailView
 
 # Create your views here.
