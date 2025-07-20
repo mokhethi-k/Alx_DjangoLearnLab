16fd71bbb55cf629fa90755c2a69b24b499c5aff
mokhethi-k <mokhethikabelo@gmail.com>
2025-07-20 21:57:34 +0200

    correct the user-role-based views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -84,24 +84,27 @@ def register(request):
     return render(request,'relationship_app/register.html', context)
 
 
+
 def is_admin(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
 
 @user_passes_test(is_admin)
 def Admin(request):
-    return render(request, 'relationship_app/admin_view.html')
-
-def is_librarian(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+    return render(request, 'relationship/admin_view.html')
 
 @user_passes_test(is_librarian)
 def Librarian(request):
-    return render(request, 'relationship_app/librarian_view.html')
-
-def is_member(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+    return render(request, 'relationship/librarian_view.html')
 
 @user_passes_test(is_member)
 def Member(request):
-    return render(request, 'relationship_app/member_view.html')
+    return render(request, 'relationship/member_view.html')
+
     
