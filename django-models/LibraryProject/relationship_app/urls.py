940bc0d7d2b47006d1c883bf82cb5f6e8fdf5745
mokhethi-k <mokhethikabelo@gmail.com>
2025-07-20 16:18:27 +0200

    change the register view to function based view

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, RegisterView
+from .views import list_books, LibraryDetailView
+from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('register/', RegisterView.as_view(), name='register'),
+    path('register/', views.register , name='register'),
     path('login/', LoginView.as_view(template_name = 'relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name = 'relationship_app/logout.html'), name='logout'),
     path('', list_books, name='book_list' ),
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -17,10 +17,8 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
-class RegisterView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'relationship_app/register.html'
+def register(request):
+    return render(request,'relationship_app/register.html')
 
 
     
