a530de8363766a7c48f0549a01c1a7e1b4fb956a
mokhethi-k <mokhethikabelo@gmail.com>
2025-07-26 18:52:01 +0200

    correct the code in bookshelf app

---
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from django.forms import TextInput, Textarea
+from django import forms
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'date_of_birth', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "publication_year")
+    list_filter = ("publication_year",)
+    search_fields = ("title", "author")
+
+admin.site.register(Book, BookAdmin)
