868e35f7d1e9ec6ad688697c6d352361c16c46f6
mokhethi-k <mokhethikabelo@gmail.com>
2025-07-29 21:21:49 +0200

    mplenting authentication and permisions in Django REST Framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, permissions
 from .models import Book
 from .serializers import BookSerializer
-# Create your views here.
 
+
+# BookList is protected by token authentication.
+# Only authenticated users can access book data.
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    pagination_class = [permissions.IsAuthenticated, permissions.AllowAny]
 
+# BookViewSet is protected by token authentication.
+# Only admin users can perform CRUD operations on the book data.
 class BookViewSet(viewsets.ModelViewSet):
     model = Book
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAdminUser]
+    
