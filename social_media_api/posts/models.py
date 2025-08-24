002e695ce8ae65f8df6c8e63ceeaf3a76e8026e5
mokhethi-k <mokhethikabelo@gmail.com>
2025-08-24 20:08:43 +0200

    implementing notification and likes functionality

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -23,3 +23,12 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author} on {self.post}"
+    
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="likes")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "post")
